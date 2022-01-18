2f20df15904f4ef0e4d085ae7290c74a3aa4bf3d
Łukasz Żmuda <l.t.zmuda@gmail.com>
2022-01-18 22:01:03 +0100

    create dump function

--- index.php
+++ index.php
@@ -1,2 +1,20 @@
+<?php
 
+echo "<h1>To działa !!!</h1>";
 
+$test = ['01', '02', '03', '04'];
+
+function dump($data)
+{
+    echo '<div style="
+    display: inline-block;
+    padding: 10px;
+    border: 1px solid black;
+    background-color: lightgray;
+    ">
+    <pre>';
+    print_r($data);
+    echo '</pre></div>';
+}
+
+dump($test);
